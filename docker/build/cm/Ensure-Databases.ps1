c403d8702cc8ddf2400439940d4991bfb18c6ee3
Per Bering <per.bering@valtech.com>
2022-02-22 11:10:54 +0100

    cleanup

--- docker/build/cm/Ensure-Databases.ps1
+++ docker/build/cm/Ensure-Databases.ps1
@@ -1,5 +1,5 @@
-$sqlInstanceName = "Sitecore"
-$sqlShareName = "SC_DB"
+$sqlInstanceName = "MSSQLLocalDB"
+$sqlShareName = "SC"
 
 sqllocaldb.exe create $sqlInstanceName
 sqllocaldb.exe share $sqlInstanceName $sqlShareName
@@ -7,7 +7,7 @@ sqllocaldb.exe start $sqlInstanceName
 
 $sqlServer = "(LocalDB)\.\$sqlShareName"
 
-sqlcmd.exe -S $sqlServer -Q "create login [IIS APPPOOL\DefaultAppPool] from windows; exec sp_addsrvrolemember 'IIS APPPOOL\DefaultAppPool', sysadmin;"
+sqlcmd.exe -S $sqlServer -Q "CREATE LOGIN [IIS APPPOOL\DefaultAppPool] FROM Windows; EXEC sp_addsrvrolemember 'IIS APPPOOL\DefaultAppPool', sysadmin;"
 sqlpackage.exe /a:Publish /sf:C:\mssql-init\resources\Sitecore.Core.dacpac /p:AllowIncompatiblePlatform=True /tdn:Sitecore.Core /tsn:$sqlServer
 sqlpackage.exe /a:Publish /sf:C:\mssql-init\resources\Sitecore.Master.dacpac /p:AllowIncompatiblePlatform=True /tdn:Sitecore.Master /tsn:$sqlServer
 sqlpackage.exe /a:Publish /sf:C:\mssql-init\resources\Sitecore.Web.dacpac /p:AllowIncompatiblePlatform=True /tdn:Sitecore.Web /tsn:$sqlServer
